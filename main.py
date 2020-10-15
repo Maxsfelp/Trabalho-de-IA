e1cf5db6a2f98fbec6fc9dc131361ca81426108d
humbertobianchini <58179449+humbertobianchini@users.noreply.github.com>
2020-10-14 22:54:06 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,33 +3,31 @@
 
 """
 Missionários e Canibais
-Autores: Felipe Maxsuel / Humberto Bianchini
+Autores: Felipe Maxusel / Humberto Bianchini
 """
 
 
 def main():
     # Formato = [Missionario, Canibais, Margem]
     possibilidades = [[1, 0, 1], [0, 1, 1], [2, 0, 1], [0, 2, 1], [1, 1, 1]]
-    estado_final = [0, 0, 0]
+    # estado_final = [0, 0, 0]
+    ja_visitados = [[3, 3, 1]]
     estado_atual = [[3, 3, 1]]
-    if estado_final in estado_atual:
-        print('nao')
-    faixa_estados = verifica_estado(possibilidades, estado_atual)
-    print(faixa_estados)
+    print(verifica_estado(possibilidades, estado_atual, ja_visitados))
+    print(ja_visitados)
 
 
-def verifica_estado(possibilidades, estado_atual):
+def verifica_estado(p, e, jv):
     novos_estados = []
-    ja_visitados = [estado_atual]
-    for i in range(len(possibilidades)):
-        estado = []
-        for j in range(len(estado_atual)):
-            for k in range(len(estado_atual[j])):
-                tmp = estado_atual[j][k] - possibilidades[i][k]
+    for i in range(len(p)):  # 0 - 1 - 2 - 3 - 4
+        for j in range(len(e)):  # 0 - 1
+            estado = []
+            for k in range(len(e[j])):  # 0 - 1 - 3
+                tmp = e[j][k] - p[i][k]
                 estado.append(tmp)
-        if estado[0] >= estado[1] and estado not in ja_visitados:
-            novos_estados.append(estado)
-            ja_visitados.append(estado)
+            if 3 >= estado[1] <= estado[0] <= 3 and estado not in jv:
+                novos_estados.append(estado)
+                jv.append(estado)
     return novos_estados
 
 
