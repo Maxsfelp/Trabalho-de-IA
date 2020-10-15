2b99481e2f540a5ab599a69374e01025a4ffc1d9
Maxsfelp <felipemaxsuel74@gmail.com>
2020-10-15 02:22:17 -0300

    Updade main

--- main.py
+++ main.py
@@ -12,9 +12,10 @@ def main():
     possibilidades = [[1, 0, 1], [0, 1, 1], [2, 0, 1], [0, 2, 1], [1, 1, 1]]
     estado_final = [0, 0, 0]
     ja_visitados = [[3, 3, 1]]
-    estado_atual = [[3, 1, 1]]
-    print(verifica_estado(possibilidades, estado_atual, ja_visitados))
-    # print(ja_visitados)
+    estado_atual = [[3, 3, 1]]
+    while estado_final not in estado_atual:
+        estado_atual = verifica_estado(possibilidades, estado_atual, ja_visitados)
+        # print(estado_atual)
 
 
 def verifica_estado(p, e, jv):
@@ -28,9 +29,10 @@ def verifica_estado(p, e, jv):
                 else:
                     tmp = e[j][k] + p[i][k]
                 estado.append(tmp)
-            if ((estado[0] == 3 or estado[0] == estado[1]) and estado not in jv and (estado[1] >= 0 and estado[1] <= 3)):
+            if ((estado[0] == 3 or estado[0] == estado[1] or estado[0] == 0) and estado not in jv and (estado[1] >= 0 and estado[1] <= 3)):
                 novos_estados.append(estado)
                 jv.append(estado)
+    print(novos_estados)
     return novos_estados
 
 
