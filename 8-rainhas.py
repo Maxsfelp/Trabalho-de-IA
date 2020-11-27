b0091a6f3994630f8c83f82dbfc1bbb0ada603a5
Maxsfelp <felipemaxsuel74@gmail.com>
2020-11-27 16:06:51 -0300

    mudanças no código

--- 8-rainhas.py
+++ 8-rainhas.py
@@ -40,30 +40,32 @@ def cliques(G):
     except IndexError:
         pass
 
-G = nx.Graph()
+if __name__ == '__main__':
+    G = nx.Graph()
 
-G.add_nodes_from(range(0, 64))
+    G.add_nodes_from(range(0, 64))
 
-for x_i in range(0,8):
-    for y_i in range(0,8):
-        for x_j in range(0,8):
-            for y_j in range(0,8):
-                if(x_i != x_j or y_i != y_j):
-                    if(x_i == x_j or y_i == y_j or abs(x_i - x_j) == abs(y_i - y_j)):
-                        G.add_edge(x_i + y_i * 8, x_j + y_j * 8)
+    for x_i in range(0,8):
+        for y_i in range(0,8):
+            for x_j in range(0,8):
+                for y_j in range(0,8):
+                    if(x_i != x_j or y_i != y_j):
+                        if(x_i == x_j or y_i == y_j or abs(x_i - x_j) == abs(y_i - y_j)):
+                            G.add_edge(x_i + y_i * 8, x_j + y_j * 8)
 
-j = 0
-GC = nx.complement(G)
-for i in cliques(GC):
-    if len(list(i)) == 8:
-        j = j + 1
-        print(i)
-print('São', j,'possbilidades')
+    j = 0
+    GC = nx.complement(G)
+    for i in cliques(GC):
+        if len(list(i)) == 8:
+            j = j + 1
+            print(i)
+
+    print('São', j,'possbilidades')
 
 
-nx.draw(G, with_labels=True, font_weight='bold', width=1)
-plt.show()
-plt.savefig("grafo.png")
-nx.draw(GC, with_labels=True, font_weight='bold', width=1)
-plt.show()
-plt.savefig("grafoComplementar.png")
+    nx.draw(G, with_labels=True, font_weight='bold', width=1)
+    plt.show()
+    plt.savefig("grafo.png")
+    nx.draw(GC, with_labels=True, font_weight='bold', width=1)
+    plt.show()
+    plt.savefig("grafoComplementar.png")
